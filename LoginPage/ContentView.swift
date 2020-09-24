2e79d769098de4973261ee4969f300cc157498cb
sxc <qiuyequ@icloud.com>
2020-09-24 14:43:35 +0800

    Initial Commit

---
+++ LoginPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginPage
+//
+//  Created by Xiaochun Shen on 2020/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Xiaochun Shen on 2020/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
