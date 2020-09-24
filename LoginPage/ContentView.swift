62ca7c2d5a38dc3bac63791614c63803dd4f32f9
sxc <qiuyequ@icloud.com>
2020-09-24 14:58:32 +0800

    add text and image

--- LoginPage/ContentView.swift
+++ LoginPage/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            WelcomeText()
+            UserImage()
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("ProfilePicture")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .padding(.bottom, 75.0)
+            .frame(width: 150.0, height: 150.0)
+            .cornerRadius(/*@START_MENU_TOKEN@*/150.0/*@END_MENU_TOKEN@*/)
     }
 }
